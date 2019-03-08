ca35812f5670eb83461887918a88454e242e550b
Annie Aasen <aasenannie@gmail.com>
2019-03-08 19:31:56 +0100

    Add map view

--- react-native/ReactApp/screens/DashboardScreen.js
+++ react-native/ReactApp/screens/DashboardScreen.js
@@ -5,6 +5,7 @@ import {
   Text,
   View,
 } from 'react-native';
+import { MapView } from 'expo';
 
 
 export default class HomeScreen extends React.Component {
@@ -13,6 +14,9 @@ export default class HomeScreen extends React.Component {
     temp: 18,
     humidity: 0.28,
     altitude: 0,
+    latitude: 37.78825,
+    longitude: -122.4324,
+    locationDelta: 0.0922,
   };
 
   static navigationOptions = {
@@ -20,18 +24,36 @@ export default class HomeScreen extends React.Component {
   };
 
   render() {
+    const {
+      latitude,
+      longitude,
+      locationDelta,
+      weight,
+      temp,
+      humidity,
+      altitude,
+    } = this.state;
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude,
+              longitude,
+              latitudeDelta: locationDelta,
+              longitudeDelta: locationDelta,
+            }}
+          />
           <View style={styles.weightContainer}>
             <Text>The weight of your suitcase is:</Text>
-            <Text style={styles.weightText}>{this.state.weight} kg</Text>
+            <Text style={styles.weightText}>{weight} kg</Text>
           </View>
 
           <View style={styles.sensorsContainer}>
-            <Text>Temperature: {this.state.temp}</Text>
-            <Text>Humidity: {this.state.humidity}</Text>
-            <Text>Altitude: {this.state.altitude} m</Text>
+            <Text>Temperature: {temp}</Text>
+            <Text>Humidity: {humidity}</Text>
+            <Text>Altitude: {altitude} m</Text>
           </View>
 
         </ScrollView>
@@ -45,9 +67,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  map: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: 300,
+    width: 300,
+  },
   weightText: {
     marginTop: 10,
-    marginBottom: 20,
     fontSize: 40,
     textAlign: 'center',
   },
