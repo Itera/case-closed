38b5ce29ca8647bc9054e3e806b98a6078daf77e
Tom Eirik Slettahjell <tom.eirik.slettahjell@itera.no>
2019-03-09 10:30:10 +0100

    Remove unused 'tare' button code from Arduino.

--- arduino/arduino_a.cpp
+++ arduino/arduino_a.cpp
@@ -3,7 +3,6 @@
 
 #define CLK 2
 #define DOUT 3
-#define BUTTON_TARE 5
 #define BUTTON_SOUND 6
 
 #define COMMAND_TARE 1
@@ -20,13 +19,9 @@ int led_state = LOW;
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(BUTTON_SOUND, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, led_state);
-
-  pinMode(BUTTON_TARE, OUTPUT);
-  pinMode(BUTTON_SOUND, INPUT);
-
   scale.set_scale(SCALE_FACTOR);
   scale.tare();
 }
@@ -38,19 +33,13 @@ void loop() {
     Serial.println("Got command:");
     Serial.println(command);
 
-    // TOOD: Execute command.
+    // Execute command.
     if (command == COMMAND_TARE) {
       scale.tare();
     } else if (command == COMMAND_LED) {
       led_state = ~led_state;
       digitalWrite(LED_BUILTIN, led_state);
     }
-  }
-
-  // Check tare button state (delay, then reset weight).
-  if (digitalRead(BUTTON_TARE) == HIGH) {
-    delay(1000);
-    scale.tare();
   }
 
   // Poll and send sensor data (very ugly).
